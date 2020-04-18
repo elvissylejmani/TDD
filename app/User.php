afd117f7b3acd5ef0a59410ce92a111124ff664b
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-19 00:00:44 +0200

    Has many relationship

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function Projects()
+    {
+        return $this->hasMany(project::class,'owner_id','id');
+    }
 }
