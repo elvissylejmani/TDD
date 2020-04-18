540fa1fefab3666107dd9e9ebedc0f8da10440d9
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-18 23:59:18 +0200

    Create factory

---
+++ database/factories/project.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\project;
+use Faker\Generator as Faker;
+
+$factory->define(project::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text,
+        'description' =>  $faker->text,
+        'owner_id' => factory(App\User::class),
+    ];
+});
