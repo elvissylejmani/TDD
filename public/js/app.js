38679409db31ce85ec88edac78971c84b107a4cb
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-22 22:52:54 +0200

    npm run dev

--- public/js/app.js
+++ public/js/app.js
@@ -49935,8 +49935,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\elvis\Desktop\Laravel\tdd\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\elvis\Desktop\Laravel\tdd\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\elvis\Desktop\Laravel\TDD\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\elvis\Desktop\Laravel\TDD\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
