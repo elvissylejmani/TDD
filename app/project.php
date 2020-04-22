68b4c9027990c297456024c0443c93665de38181
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-22 17:59:43 +0200

    path created at model

--- app/project.php
+++ app/project.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class project extends Model
 {
     protected $fillable = ['title','description','owner_id'];
+
+
+    public function path()
+    {
+        return "/Projects/{$this->id}";
+    }
 }
