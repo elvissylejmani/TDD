4774436033a1bb0f80e3e9128c09be00b1fcd574
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-23 23:16:22 +0200

    tailwind css added to views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,23 +19,17 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="bg-gray-300">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+        <nav class="bg-white">
+            <div class="container mx-auto">
+                <div class="flex justify-between items-center py-2">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
+               
+                <div>
+                   
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -70,9 +64,10 @@
                     </ul>
                 </div>
             </div>
+            </div>
         </nav>
 
-        <main class="container py-4">
+        <main class="container mx-auto  py-4">
             @yield('content')
         </main>
     </div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,29 @@
 @extends('../layouts.app')
 
 @section('content')
-<div style="display: flex; align-items: center">
-    <h1 style="margin-right: auto">BirdBoard</h1>
+<div class="flex items-center mb-3">
     <a href="/Projects/create">New Project</a>
 </div>
-@forelse ($projects as $project)
-<li>
-<a href="{{$project->path()}}">    
-    {{ $project->title}}
-</a>
-</li>
-@empty
-No Project yet.
 
+<div class="flex flex-wrap -mx-3">
+@forelse ($projects as $project)
+<div class="w-1/3 px-3 pb-6">
+    <div class="bg-white p-5 rounded shadow " style="height:200px">
+        <h3 class="font-normal text-xl py-4">{{ \Illuminate\Support\Str::limit($project->title, 50  ) }}</h3>
+        <div class="text-gray-600">
+            {{ \Illuminate\Support\Str::limit($project->description, 100) }}
+        </div>
+    </div>
+</div>
+    @empty
+    <div>
+No Project yet.
+</div>
 @endforelse
+
+</div>
+
+
 
 
 @endsection
