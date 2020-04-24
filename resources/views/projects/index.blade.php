559083d12a89ad497b29d3aa9a67d3688f453568
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-24 14:57:56 +0200

    new style

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <header class="flex items-center mb-3 py-3">
-    <div class="flex justify-between items-center w-full">
+    <div class="flex justify-between items-end w-full">
     <h2 class="text-gray-600 font-normal">My Projects</h2>
     <a href="/Projects/create" class="button">New Project</a>
     </div>
@@ -11,7 +11,7 @@
 <main class="lg:flex flex-wrap -mx-3">
 @forelse ($projects as $project)
 <div class="lg:w-1/3 px-3 pb-6">
-    <div class="bg-white p-5 rounded-lg shadow " style="height:200px">
+    <div class="card" style="height:200px">
         <h3 class="font-normal text-xl py-4 mb-3 -ml-5 border-l-4 border-blue-400 pl-4">
         <a href="{{ $project->path()}}">    {{ \Illuminate\Support\Str::limit($project->title, 50  ) }} </a>
         </h3>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
     
+<header class="flex items-center mb-3 py-3">
+    <div class="flex justify-between items-end w-full">
+    <h2 class="text-gray-600 font-normal">My Projects</h2>
+    <a href="/Projects/create" class="button">New Project</a>
+    </div>
+</header>
+
+<main>
+    <div></div>
+    <div></div>
+</main>
+
+
 <h1>{{$project->title}}</h1>
 
 <p>{{$project->description}}</p>
