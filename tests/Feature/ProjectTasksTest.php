a6dbba160ade17183f26a923e9ffdff9bf4fefa3
elvis sylejmani <elvissylejmani13@gmail.com>
2020-04-25 16:31:58 +0200

    a project can have tasks test implemented

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -13,7 +13,16 @@ class ProjectTasksTest extends TestCase
     /** @test */
     public function a_project_can_have_tasks()
     {
+        $this->withoutExceptionHandling();
         
+        $this->signIn();
+
+        $project = factory('App\project')->create(['owner_id' => auth()->id()]);
+
+        $this->post($project->path() . 'tasks', ['body' => 'test task']);
+
+        $this->get('/Projects' . $project->path())
+        ->assertSee('test task');
     }
 
 }
